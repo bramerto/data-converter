ccb93f25889af552979202823d75811d1411c93b
Bram <bramerto@users.noreply.github.com>
2018-01-25 17:54:04 +0100

    Initial commit

---
+++ data-converter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+
+namespace dataconverter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            SQLprovider sql = new SQLprovider(); 
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+            Microsoft.Office.Interop.Excel.Range oRng;
+
+            oXL = new Microsoft.Office.Interop.Excel.Application();
+            oXL.Visible = true;
+            oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(""));
+            oSheet = (Microsoft.Office.Interop.Excel._Worksheet)oWB.ActiveSheet;
+
+            object misvalue = System.Reflection.Missing.Value;
+            DataTable t = sql.PullData("AuthAssignment");
+            DataRow firstrow = t.Rows[0];
+
+            int columnCounter = 0;
+            foreach (System.Data.DataColumn col in firstrow.Table.Columns) {
+                oSheet.Cells[1, columnCounter] = col.ToString();
+                Console.Write(col + " | ");
+            }
+            Console.WriteLine(" ");
+
+            foreach(System.Data.DataRow row in t.Rows) {
+                foreach(Object item in row.ItemArray) {
+                    if (item != null || (string)item != string.Empty) {
+                        Console.Write(item + " | ");
+                    }
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ data-converter/SQLprovider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace dataconverter
+{
+    public class SQLprovider
+    {
+        private MySqlConnectionStringBuilder conn_string;
+        DataTable dataTable;
+
+        public SQLprovider()
+        {
+            conn_string = new MySqlConnectionStringBuilder();
+            conn_string.Server = "127.0.0.1";
+            conn_string.UserID = "root";
+            conn_string.Password = "";
+            conn_string.Database = "conversion";
+            dataTable = new DataTable();
+        }
+
+        public DataTable PullData(String table)
+        {
+            String query = "SELECT * FROM " + table;
+            MySqlConnection conn = new MySqlConnection(conn_string.ToString());
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            try {
+                conn.Open();
+                Console.WriteLine("Connection Open");
+                // create data adapter
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                // this will query your database and return the result to your datatable
+                da.Fill(dataTable);
+                conn.Close();
+                da.Dispose();
+
+                Console.WriteLine("Query executed");
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+
+            return dataTable;
+        }
+    }
+}
